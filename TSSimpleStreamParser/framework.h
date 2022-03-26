91df747d2fce9c5a8d35e18511c5964370e5da37
f0reachARR <f0reach@f0reach.me>
2022-03-26 13:22:05 +0900

    :+1: add parser project

--- TSSimpleStreamParser/framework.h
+++ TSSimpleStreamParser/framework.h
@@ -1,3 +1,6 @@
 ﻿#pragma once
 
-#define WIN32_LEAN_AND_MEAN             // Windows ヘッダーからほとんど使用されていない部分を除外する
+#define WIN32_LEAN_AND_MEAN  // Windows
+                             // ヘッダーからほとんど使用されていない部分を除外する
+#include <cstdlib>
+#include <string>
