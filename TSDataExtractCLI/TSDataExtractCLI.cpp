016ef147d748e155b9f3af0c965c2fc5687c75e5
f0reachARR <f0reach@f0reach.me>
2022-03-31 16:39:23 +0900

    :sparkles: テスト用CLIを追加 1

---
+++ TSDataExtractCLI/TSDataExtractCLI.cpp
@@ -0,0 +1,49 @@
+﻿// TSDataExtractCLI.cpp : このファイルには 'main'
+// 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
+//
+
+#include <BmlParserFilter.h>
+#include <Engine/StreamSourceEngine.hpp>
+#include <Filters/StreamSourceFilter.hpp>
+#include <Filters/TSPacketParserFilter.hpp>
+#include <LibISDB.hpp>
+#include <TS/TSDownload.hpp>
+#include <Utilities/Utilities.hpp>
+#include <iostream>
+
+class ISDBBmlParserEngine : public LibISDB::StreamSourceEngine,
+                            public BmlModuleHandler {
+  void OnModuleListUpdate(const BmlModuleInfo& Data) override {}
+  void OnModuleDownload(const BmlModule& Data) override {}
+};
+
+#if defined(LIBISDB_WCHAR)
+int wmain(int argc, wchar_t** argv) {
+#else
+int main(int argc, char** argv) {
+#endif
+  if (argc < 2) {
+    std::cerr << "Need filename." << std::endl;
+    return 1;
+  }
+
+  LibISDB::StreamSourceFilter* pSource = new LibISDB::StreamSourceFilter;
+  LibISDB::TSPacketParserFilter* pParser = new LibISDB::TSPacketParserFilter;
+  BmlParserFilter* pLogoDownloader = new BmlParserFilter;
+
+  ISDBBmlParserEngine Engine;
+
+  Engine.BuildEngine({pSource, pParser, pLogoDownloader});
+  Engine.SetStartStreamingOnSourceOpen(true);
+
+  pLogoDownloader->SetModuleHandler(&Engine);
+
+  const LibISDB::CharType* pFileName = argv[1];
+  if (!Engine.OpenSource(pFileName)) {
+    std::cerr << "Failed to open file : " << pFileName << std::endl;
+    return 1;
+  }
+
+  Engine.WaitForEndOfStream();
+  Engine.CloseSource();
+}
